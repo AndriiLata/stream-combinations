2473eefb22e10abc5db80b0bde1a7ea4325d6af5
Andrii Lata <lataandrii@gmail.com>
2024-11-13 16:12:10 +0100

    test

---
+++ src/test/java/com/example/streamcombinations/StreamCombinationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.streamcombinations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamCombinationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
