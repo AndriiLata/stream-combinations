af333fe14968758723d51b5ae90ee5fbb0a9f9ad
Andrii Lata <lataandrii@gmail.com>
2024-12-20 22:19:36 +0100

    commit before docker

--- backend/src/main/java/com/gendev/streamcombinations/StreamCombinationsApplication.java
+++ backend/src/main/java/com/gendev/streamcombinations/StreamCombinationsApplication.java
@@ -16,32 +16,4 @@ public class StreamCombinationsApplication {
 }
 
 
-//        FetchData fetchData = new FetchData();
-//        GameFetch gameFetch = new GameFetch(fetchData.getGames());
-//        StreamingOfferFetch streamingOfferFetch = new StreamingOfferFetch(fetchData.getStreamingOffers());
-//        StreamingPackageFetch streamingPackageFetch = new StreamingPackageFetch(fetchData.getStreamingPackages());
-//        StreamingService sS = new StreamingService(gameFetch, streamingOfferFetch, streamingPackageFetch);
-//
-//        Set<String> teams = Set.of("Schweiz");
-//       Set<String> tournaments = Set.of("UEFA Champions League 24/25");
-////        LocalDateTime startDate = DateUtils.parse("2023-08-27 15:00:00");
-////        LocalDateTime endDate = DateUtils.parse("2023-08-27 17:00:00");
-//
-//        Set<Game> games = sS.getRequiredGames(teams, null, null, null);
-//
-//        List<StreamingPackage> streamingPackages = sS.findCheapestCombination(games);
-//        List<StreamingPackage> streamingPackages = sS.findLeastServicesCombination(games);
-//
-//        Map<StreamingPackage, Set<GameOffer>> btg = sS.buildPackageToGameOffers(games);
-//
-//
-//
-//        for (StreamingPackage streamingPackage : streamingPackages) {
-//            System.out.println("Streaming package: " + streamingPackage.getName());
-//            System.out.println(btg.get(streamingPackage).size());
-//            btg.get(streamingPackage).forEach(gameOffer -> {
-//                System.out.println("Game: " + gameOffer.getGame().getId()+ " with live: " + gameOffer.isLive() + " and highlights: " + gameOffer.isHighlights());
-////                System.out.println("Game: " + gameOffer.getGame().getId());
-//            });
-//        }
 
--- backend/src/main/java/com/gendev/streamcombinations/util/FetchData.java
+++ backend/src/main/java/com/gendev/streamcombinations/util/FetchData.java
@@ -27,37 +27,4 @@ public class FetchData {
         this.teamCountries = loader.loadTeamCountryFromCSV("dataset/teams_countries.csv");
     }
 
-
-
-
-    //THIS IS JUST FOR TESTING
-//    public void printGames() {
-//        if (games != null && !games.isEmpty()) {
-//            for (Game game : games) {
-//                System.out.println(game.getTeam_away() + " vs " + game.getTeam_home() + " on " + game.getStarts_at());
-//            }
-//        } else {
-//            System.out.println("No games found!");
-//        }
-//    }
-//    //print streaming offers
-//    public void printStreamingOffers() {
-//        if (streamingOffers != null && !streamingOffers.isEmpty()) {
-//            for (StreamingOffer streamingOffer : streamingOffers) {
-//                System.out.println("Streaming offer: " + streamingOffer.getStreaming_package_id() + " with " + streamingOffer.getLive() + " live and " + streamingOffer.getHighlights() + " highlights");
-//            }
-//        } else {
-//            System.out.println("No streaming offers found!");
-//        }
-//    }
-//    //print streaming packages
-//    public void printStreamingPackages() {
-//        if (streamingPackages != null && !streamingPackages.isEmpty()) {
-//            for (StreamingPackage streamingPackage : streamingPackages) {
-//                System.out.println("Streaming package: " + streamingPackage.getName() + " with monthly price " + streamingPackage.getMonthly_price_cents() + " and yearly subscription price " + streamingPackage.getMonthly_price_yearly_subscription_in_cents());
-//            }
-//        } else {
-//            System.out.println("No streaming packages found!");
-//        }
-//    }
 }
